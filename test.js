1f78035c9af46b0a6cd9f5735071a9eebe445f2c
Ian Sinnott <ian@iansinnott.com>
2016-02-03 15:21:52 -0800

    Update tests

--- test.js
+++ test.js
@@ -1,6 +1,25 @@
 import test from 'ava';
-import fn from './';
+import replaceString from './';
 
-test('title', t => {
-	t.is(fn('unicorns'), 'unicorns & rainbows');
+test('Throws if not given a non-empty string', t => {
+  t.throws(() => replaceString());
+  t.throws(() => replaceString(''));
+});
+
+test('Returns an array', t => {
+  t.true(Array.isArray(replaceString('blah', 'blah', x => x)));
+});
+
+test('Works with strings', t => {
+  t.same(
+    replaceString('hey there', 'hey', x => ({ worked: x })),
+    ['', { worked: 'hey' }, ' there']
+  );
+});
+
+test('Works with matching groups', t => {
+  t.same(
+    replaceString('hey there', /(hey)/g, x => ({ worked: x })),
+    ['', { worked: 'hey' }, ' there']
+  );
 });
