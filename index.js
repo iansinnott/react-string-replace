502b5d91b1f5d341d1a13931769ecd7264fce328
Ian Sinnott <ian@iansinnott.com>
2016-02-03 15:20:30 -0800

    Remove unecessary files and add main function

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
+/* eslint-disable vars-on-top, no-var, prefer-template */
 'use strict';
-module.exports = function (str, opts) {
-	if (typeof str !== 'string') {
-		throw new TypeError('Expected a string');
-	}
 
-	opts = opts || {};
+var isRegExp = require('lodash.isregexp');
+var escapeRegExp = require('lodash.escaperegexp');
 
-	return str + ' & ' + (opts.postfix || 'rainbows');
+/**
+ * Given a string, replace every substring that is matched by the `match` regex
+ * with the result of calling `fn` on matched substring. The result will be an
+ * array with all odd indexed elements containing the replacements. The primary
+ * use case is similar to using String.prototype.replace execpt for React.
+ *
+ * React will happily render an array as children of a react element, which
+ * makes this approach very useful for tasks like surrounding certain text
+ * within a string with react elements.
+ *
+ * Example:
+ * matchReplace(
+ *   'Emphasize all phone numbers like 884-555-4443.',
+ *   /([\d|-]+)/g,
+ *   (number, i) => <strong key={i}>{number}</strong>
+ * );
+ * // => ['Emphasize all phone numbers like ', <strong>884-555-4443</strong>, '.'
+ *
+ * @param {string} str
+ * @param {regexp|str} match Must contain a matching group
+ * @param {function} fn
+ * @return {array}
+ */
+module.exports = function reactReplaceString(str, match, fn) {
+  if (typeof str !== 'string' || !str) {
+    throw new TypeError('First argument to react-replace-string must be a non-empty string');
+  }
+
+  var re = match;
+
+  if (!isRegExp(re)) {
+    re = new RegExp('(' + escapeRegExp(re) + ')', 'gi');
+  }
+
+  var result = str.split(re);
+
+  // Apply fn to all odd elements
+  for (var i = 1, length = result.length; i < length; i += 2) {
+    result[i] = fn(result[i], i);
+  }
+
+  return result;
 };
